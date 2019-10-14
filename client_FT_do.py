7d1c92e3cfa36d29a1b398bcd31231e63fd98c14
JJ0023 <43746106+JJ0023@users.noreply.github.com>
2019-10-14 10:23:08 +0530

    Add files via upload

---
+++ client_FT_do.py
@@ -0,0 +1,17 @@
+import socket                   
+
+if __name__ == "__main__":
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host =(input("Enter server ip: "))
+    port = int(input("Enter port: "))
+
+    serversocket.connect((host, port))
+    filename = "recieved"
+    with open(filename, 'wb') as f:
+        while True:
+            print("Recieving Data")
+            data = serversocket.recv(1024)
+            if not data:
+                break
+            f.write(data)
+        f.close
---
+++ server_FT_do.py
@@ -0,0 +1,26 @@
+import socket                   
+
+if __name__ == "__main__":
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host =(input("Set server ip: "))
+    port = int(input("Set port: "))
+
+    serversocket.bind((host, port))
+    serversocket.listen(5)
+    print("Sender is ready and listening")
+
+    clientsocket, address = serversocket.accept()
+    print("Reciever " + str(address) + " connected")
+    filename = str(input("Enter file name: "))
+    #clientsocket.send(filename)
+
+    f = open(filename, 'rb')
+    buffer = f.read(1024)
+    while(buffer):
+        clientsocket.send(buffer)
+        buffer = f.read(1024)
+    f.close()
+
+    print("Done Sending")
+
+
